777719b8468805f53aa4398c8ed1877fe3bcc927
Drew-Garvey <artman.drew@gmail.com>
2014-12-09 01:29:01 -0500

    fixed IE SVG bug with modernizr

--- js/build.js
+++ js/build.js
@@ -37,7 +37,18 @@ $(function() {
     }
     });
 
+  // Modernizr - SVG/PNG IE image fallback.
+  if (!Modernizr.svg) {
+      var imgs = document.getElementsByTagName('img');
+      var svgExtension = /.*\.svg$/
+      var l = imgs.length;
+      for(var i = 0; i < l; i++) {
+          if(imgs[i].src.match(svgExtension)) {
+              imgs[i].src = imgs[i].src.slice(0, -3) + 'png';
+              console.log(imgs[i].src);
+          }
+      }
+  }
 
-		
 });
 
